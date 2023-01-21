120b3b5f439bc962c27441b3370a91181d7a65cd
Ximeen <gabriximenes100@gmail.com>
2023-01-21 10:39:50 -0300

    create route /summary with queryRaw

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,6 @@ import { prisma } from "./lib/prisma";
 import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import dayjs from "dayjs";
-import { request } from "http";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/habits", async (request) => {
@@ -112,5 +111,33 @@ export async function appRoutes(app: FastifyInstance) {
         },
       });
     }
+  });
+
+  app.get("/summary", async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM days_habits DH
+          WHERE
+            DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+
+    `;
+
+    return summary;
   });
 }
