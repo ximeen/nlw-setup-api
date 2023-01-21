9de004442ac3b862ecb6954b618c7f6acd28c498
Ximeen <gabriximenes100@gmail.com>
2023-01-20 21:36:28 -0300

    create route toggle habits

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { prisma } from "./lib/prisma";
 import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import dayjs from "dayjs";
+import { request } from "http";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/habits", async (request) => {
@@ -64,5 +65,52 @@ export async function appRoutes(app: FastifyInstance) {
       return dayHabit.habit_id;
     });
     return { possibleHabits, completedHabits };
+  });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
   });
 }
