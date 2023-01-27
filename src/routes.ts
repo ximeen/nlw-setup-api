894747e39f26048deee4d5ae65f4f9b943d56ffe
Ximeen <gabriximenes100@gmail.com>
2023-01-27 06:35:49 -0300

    fix bug route /day

--- src/routes.ts
+++ src/routes.ts
@@ -60,9 +60,10 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
 
-    const completedHabits = day?.dayHabits.map((dayHabit) => {
-      return dayHabit.habit_id;
-    });
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => {
+        return dayHabit.habit_id;
+      }) ?? [];
     return { possibleHabits, completedHabits };
   });
 
@@ -137,7 +138,6 @@ export async function appRoutes(app: FastifyInstance) {
       FROM days D
 
     `;
-
     return summary;
   });
 }
